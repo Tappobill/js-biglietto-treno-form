6f185bfa6ad1f063f622c4fb94e70b40da8bd9e7
Tappobill <christian.salvati@hotmail.it>
2022-10-20 19:43:58 +0200

    Script

--- jscript/main.js
+++ jscript/main.js
@@ -1,9 +1,20 @@
-let inputKm = document.getElementById("inputKm");
-let inputEta = document.getElementById("inputEta");
-
-document.getElementById("title").innerHTML = `<h1>CALCOLA IL PREZZO DEL TUO BIGLIETTO</h1>`
-
 function recuperoDati() {
+    let inputKm = document.getElementById("inputKm");
+    let inputEta = document.getElementById("inputEta");
     let km = parseInt(inputKm.value);
+    let eta = parseInt(inputEta.value);
+    let ticketvalue = km * 0.21;
     console.log(km)
+
+    if (eta < 18) {
+        ticketvalue=ticketvalue - (ticketvalue * 0.20)
+        console.log(ticketvalue)
+    }
+    else if(eta > 65) {
+        ticketvalue=ticketvalue - (ticketvalue * 0.40)
+        console.log(ticketvalue)
+    }
+
+    
+    document.getElementById("boxvalue").innerHTML = `Il prezzo del biglietto è: ${ticketvalue}`
 }
