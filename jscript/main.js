0d3f11f655f6d67eeb9104772d3b7dd5386fefdb
Tappobill <christian.salvati@hotmail.it>
2022-10-20 20:40:21 +0200

    Primo Milestone e Form html

--- jscript/main.js
+++ jscript/main.js
@@ -7,14 +7,14 @@ function recuperoDati() {
     console.log(km)
 
     if (eta < 18) {
-        ticketvalue=ticketvalue - (ticketvalue * 0.20)
+        ticketvalue = ticketvalue - (ticketvalue * 0.20)
         console.log(ticketvalue)
     }
-    else if(eta > 65) {
-        ticketvalue=ticketvalue - (ticketvalue * 0.40)
+    else if (eta > 65) {
+        ticketvalue = ticketvalue - (ticketvalue * 0.40)
         console.log(ticketvalue)
     }
 
-    
+
     document.getElementById("boxvalue").innerHTML = `Il prezzo del biglietto è: ${ticketvalue}`
 }
